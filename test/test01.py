d936e6458f1763ba366ef92c039d5435e55a84d4
lixintian <lixintian@163.com>
2018-05-24 20:30:19 +0800

    'copyright1.4'

--- test/test01.py
+++ test/test01.py
@@ -1,1 +1,10 @@
-str = 'hello test01'
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/index')
+def index():
+    return 'index'
+
+if __name__ == '__main__':
+    app.run(debug=True)
